50d8e09685cd3960b5604d1c763bc74df05a91dc
zhaoxu98 <zhaohsu98@gmail.com>
2023-08-23 13:38:10 +0800

    first commit

---
+++ precache-manifest.acc2c9c677bc35d7f3366f6b93d60384.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee98d59e4528ba85542c2458474518b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2627edf54866add377fe",
+    "url": "/static/css/2.3f3090d9.chunk.css"
+  },
+  {
+    "revision": "df37b77a3b91364db658",
+    "url": "/static/css/main.81957205.chunk.css"
+  },
+  {
+    "revision": "2627edf54866add377fe",
+    "url": "/static/js/2.a2c44433.chunk.js"
+  },
+  {
+    "revision": "003dc047abb130b3f80a47fba06b3c90",
+    "url": "/static/js/2.a2c44433.chunk.js.LICENSE"
+  },
+  {
+    "revision": "df37b77a3b91364db658",
+    "url": "/static/js/main.fba791e7.chunk.js"
+  },
+  {
+    "revision": "7fe12fb4ac404578596f",
+    "url": "/static/js/runtime-main.65208d32.js"
+  },
+  {
+    "revision": "f1ed6197648cdfb8142b5b543898a582",
+    "url": "/static/media/logo.f1ed6197.png"
+  }
+]);
